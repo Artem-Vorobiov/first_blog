11a8e7a4f0c217c57c3dc012e2ec63426763eb6e
Artem-Vorobiov <artem-vorobiov_pavlovich@mail.ru>
2016-08-07 19:03:09 +0300

    Done with PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
 	def index
 	end
-	
+
 	def new
 	  @post = Post.new
 	end
@@ -16,9 +18,32 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Hazzahh"
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
 	private
 
 	def post_params
 		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
 	end
 end
