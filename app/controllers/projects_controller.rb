5b92b90eabba320d808f7043fb5189c847a03f1e
Artem-Vorobiov <artem-vorobiov_pavlovich@mail.ru>
2016-08-10 18:48:55 +0300

    Setup & styly basic functionality

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,52 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@projects = Project.all.paginate(:page => params[:page], per_page: 2)
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new project_params
+
+		if @project.save
+			redirect_to @project, notice: "Good for you"
+		else
+			render 'new', notice: "Bad"
+		end
+	end
+
+	def show
+
+	end
+
+	def edit
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Hazzahh"
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:title, :description, :link)
+	end
+
+	def find_project
+		@project = Project.find(params[:id])
+	end
+
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
+		@projects = Project.all.limit(3).order("created_at")
 		@posts = Post.all.limit(3).order("created_at")
 	end	
 
