ae0829baae82a70c49521fc83967add64c63ee97
Artem-Vorobiov <artem-vorobiov_pavlovich@mail.ru>
2016-08-11 20:51:08 +0300

    error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: https://quiet-hamlet-19478.herokuapp.com}
+  config.action_mailer.default_url_options = { host: 'https://quiet-hamlet-19478.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
